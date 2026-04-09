df887a7bbddc64271a7b2ce6ef0207bc64451fe5
asidhu61 <117223233+asidhu61@users.noreply.github.com>
2026-04-09 01:52:27 -0500

    Update App.java

--- View/src/main/java/sudoku/view/App.java
+++ View/src/main/java/sudoku/view/App.java
@@ -33,7 +33,7 @@ public class App extends Application {
         Parent root = loader.load();
 
         primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("Sudoku Game");
+        primaryStage.setTitle("Arshinder's Sudoku Game");
         primaryStage.show();
         primaryStage.getIcons().add(icon);
     }
